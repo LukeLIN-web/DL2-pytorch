9f0c723150d5816cdb1a00231e6c4ce5dfa009ca
LukeLIN-web <1263810658@qq.com>
2021-09-02 23:28:38 +0800

    I find a bug, stats_qs[agent] dont have any states

--- agent.py
+++ agent.py
@@ -10,10 +10,10 @@ import numpy as np
 import network
 import os
 import trace
+from torch.utils.tensorboard import SummaryWriter
 
 
-
-def collect_stats(stats_qs, step):
+def collect_stats(stats_qs, writer, step):
     policy_entropys = []
     policy_losses = []
     value_losses = []
@@ -30,22 +30,37 @@ def collect_stats(stats_qs, step):
         while not stats_qs[agent].empty():
             stats = stats_qs[agent].get()
             tag_prefix = "SAgent " + str(agent) + " "
+            print(stats[0])
             if stats[0] == "step:sl":
                 _, entropy, loss = stats
                 policy_entropys.append(entropy)
                 policy_losses.append(loss)
+                if agent < pm.NUM_RECORD_AGENTS and pm.EXPERIMENT_NAME is None:
+                    writer.add_scalar(tag=tag_prefix + "SL Loss", value=loss, step=step)
+                    writer.add_scalar(tag=tag_prefix + "SL Entropy", value=entropy, step=step)
             elif stats[0] == "val":
                 _, val_loss, jct, makespan, reward = stats
                 val_losses.append(val_loss)
                 val_jcts.append(jct)
                 val_makespans.append(makespan)
                 val_rewards.append(reward)
+            if id < pm.NUM_RECORD_AGENTS and pm.EXPERIMENT_NAME is None:
+                writer.add_scalar(tag=tag_prefix + "Val Loss", value=val_loss, step=step)
+                writer.add_scalar(tag=tag_prefix + "Val JCT", value=jct, step=step)
+                writer.add_scalar(tag=tag_prefix + "Val Makespan", value=makespan, step=step)
+                writer.add_scalar(tag=tag_prefix + "Val Reward", value=reward, step=step)
             elif stats[0] == "step:policy":
                 _, entropy, loss, td_loss, step_reward, output = stats
                 policy_entropys.append(entropy)
                 policy_losses.append(loss)
                 td_losses.append(td_loss)
                 step_rewards.append(step_reward)
+                if id < pm.NUM_RECORD_AGENTS and pm.EXPERIMENT_NAME is None:
+                    writer.add_scalar(tag=tag_prefix + "Policy Entropy", value=entropy, step=step)
+                    writer.add_scalar(tag=tag_prefix + "Policy Loss", value=loss, step=step)
+                    writer.add_scalar(tag=tag_prefix + "TD Loss", value=td_loss, step=step)
+                    writer.add_scalar(tag=tag_prefix + "Step Reward", value=step_reward, step=step)
+                    writer.add_histogram(tag=tag_prefix + "Output", value=output, step=step)
             elif stats[0] == "step:policy+value":
                 _, entropy, policy_loss, value_loss, td_loss, step_reward, output = stats
                 policy_entropys.append(entropy)
@@ -53,14 +68,66 @@ def collect_stats(stats_qs, step):
                 value_losses.append(value_loss)
                 td_losses.append(td_loss)
                 step_rewards.append(step_reward)
+                if id < pm.NUM_RECORD_AGENTS and pm.EXPERIMENT_NAME is None:
+                    writer.add_scalar(tag=tag_prefix + "Policy Entropy", value=entropy, step=step)
+                    writer.add_scalar(tag=tag_prefix + "Policy Loss", value=policy_loss, step=step)
+                    writer.add_scalar(tag=tag_prefix + "Value Loss", value=value_loss, step=step)
+                    writer.add_scalar(tag=tag_prefix + "TD Loss", value=td_loss, step=step)
+                    writer.add_scalar(tag=tag_prefix + "Step Reward", value=step_reward, step=step)
+                    writer.add_histogram(tag=tag_prefix + "Output", value=output, step=step)
             elif stats[0] == "trace:sched_result":
                 _, jct, makespan, reward = stats
                 jcts.append(jct)
                 makespans.append(makespan)
                 rewards.append(reward)
+                if id < pm.NUM_RECORD_AGENTS and pm.EXPERIMENT_NAME is None:
+                    writer.add_scalar(tag=tag_prefix + "Avg JCT", value=jct, step=step)
+                    writer.add_scalar(tag=tag_prefix + "Makespan", value=makespan, step=step)
+                    writer.add_scalar(tag=tag_prefix + "Reward", value=reward, step=step)
             elif stats[0] == "trace:job_stats":
                 _, episode, jobstats = stats
+                if id < pm.NUM_RECORD_AGENTS and pm.EXPERIMENT_NAME is None:
+                    job_stats_tag_prefix = tag_prefix + "Trace " + str(episode) + " Step " + str(step) + " "
+                    for i in range(len(jobstats["arrival"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "Arrival", value=jobstats["arrival"][i], step=i)
+                    for i in range(len(jobstats["ts_completed"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "Ts_completed",
+                                             value=jobstats["ts_completed"][i], step=i)
+                    for i in range(len(jobstats["tot_completed"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "Tot_completed",
+                                             value=jobstats["tot_completed"][i], step=i)
+                    for i in range(len(jobstats["uncompleted"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "Uncompleted", value=jobstats["uncompleted"][i],
+                                             step=i)
+                    for i in range(len(jobstats["running"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "Running", value=jobstats["running"][i], step=i)
+                    for i in range(len(jobstats["total"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "Total jobs", value=jobstats["total"][i],
+                                             step=i)
+                    for i in range(len(jobstats["backlog"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "Backlog", value=jobstats["backlog"][i], step=i)
+                    for i in range(len(jobstats["cpu_util"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "CPU_Util", value=jobstats["cpu_util"][i],
+                                             step=i)
+                    for i in range(len(jobstats["gpu_util"])):
+                        writer.add_scalar(tag=job_stats_tag_prefix + "GPU_Util", value=jobstats["gpu_util"][i],
+                                             step=i)
+                    writer.add_histogram(tag=job_stats_tag_prefix + "JCT", value=jobstats["duration"], step=step)
+
+    tag_prefix = "Central "
+    if len(policy_entropys) > 0:
+        writer.add_scalar(tag=tag_prefix + "Policy Entropy", value=sum(policy_entropys) / len(policy_entropys),
+                             step=step)
+    if len(policy_losses) > 0:
+        writer.add_scalar(tag=tag_prefix + "Policy Loss", value=sum(policy_losses) / len(policy_losses), step=step)
+    if len(value_losses) > 0:
+        writer.add_scalar(tag=tag_prefix + "Value Loss", value=sum(value_losses) / len(value_losses), step=step)
+    if len(td_losses) > 0:
+        writer.add_scalar(tag=tag_prefix + "TD Loss / Advantage", value=sum(td_losses) / len(td_losses), step=step)
+    if len(step_rewards) > 0:
+        writer.add_scalar(tag=tag_prefix + "Batch Reward", value=sum(step_rewards) / len(step_rewards), step=step)
     if len(jcts) > 0:
+        writer.add_scalar(tag=tag_prefix + "JCT", value=sum(jcts) / len(jcts), step=step)
         # log results
         if pm.TRAINING_MODE == "SL":
             f = open(LOG_DIR + "sl_train_jct.txt", 'a')
@@ -69,6 +136,7 @@ def collect_stats(stats_qs, step):
         f.write("step " + str(step) + ": " + str(sum(jcts) / len(jcts)) + "\n")
         f.close()
     if len(makespans) > 0:
+        writer.add_scalar(tag=tag_prefix + "Makespan", value=sum(makespans) / len(makespans), step=step)
         # log results
         if pm.TRAINING_MODE == "SL":
             f = open(LOG_DIR + "sl_train_makespan.txt", 'a')
@@ -76,9 +144,17 @@ def collect_stats(stats_qs, step):
             f = open(LOG_DIR + "rl_train_makespan.txt", 'a')
         f.write("step " + str(step) + ": " + str(sum(makespans) / len(makespans)) + "\n")
         f.close()
-
-
-
+    if len(rewards) > 0:
+        writer.add_scalar(tag=tag_prefix + "Reward", value=sum(rewards) / len(rewards), step=step)
+    if len(val_losses) > 0:
+        writer.add_scalar(tag=tag_prefix + "Val Loss", value=sum(val_losses) / len(val_losses), step=step)
+    if len(val_jcts) > 0:
+        writer.add_scalar(tag=tag_prefix + "Val JCT", value=sum(val_jcts) / len(val_jcts), step=step)
+    if len(val_makespans) > 0:
+        writer.add_scalar(tag=tag_prefix + "Val Makespan", value=sum(val_makespans) / len(val_makespans), step=step)
+    if len(val_rewards) > 0:
+        writer.add_scalar(tag=tag_prefix + "Val Reward", value=sum(val_rewards) / len(val_rewards), step=step)
+    writer.flush()
 
 
 def test(policy_net, validation_traces, logger, step):
@@ -116,6 +192,8 @@ def central_agent(net_weights_qs, net_gradients_qs, stats_qs):
         np.random.seed(pm.np_seed)
         torch.manual_seed(pm.tr_seed)  # specific gpu use:torch.cuda.manual_seed(seed)
 
+    writer = SummaryWriter(pm.SUMMARY_DIR)
+
     policy_net = network.PolicyNetwork("policy_net", pm.TRAINING_MODE, logger)
     if pm.VALUE_NET:
         value_net = network.ValueNetwork("value_net", pm.TRAINING_MODE, logger)
@@ -136,6 +214,7 @@ def central_agent(net_weights_qs, net_gradients_qs, stats_qs):
     start_t = time.time()  # there might be occur problem , maybe I need move it to train.py
 
     if pm.VAL_ON_MASTER:
+        logger.info("start validation for heuristics and initialized NN.")  # It could begin working.
         validation_traces = []  # validation traces
         tags_prefix = ["DRF: ", "SRTF: ", "FIFO: ", "Tetris: ", "Optimus: "]
         for i in range(pm.VAL_DATASET):
@@ -144,7 +223,6 @@ def central_agent(net_weights_qs, net_gradients_qs, stats_qs):
         stats = []
         # deep copy to avoid changes to validation_traces
         if not pm.SKIP_FIRST_VAL:
-            print("tst")
             stats.append(test(policy_net, copy.deepcopy(validation_traces), logger, step=0))  # still have some problem
             tags_prefix.append("Init_NN: ")
 
@@ -178,7 +256,9 @@ def central_agent(net_weights_qs, net_gradients_qs, stats_qs):
 
             # statistics
 
-            collect_stats(stats_qs, step)
+            collect_stats(stats_qs, writer, step)
+            writer.close()
+            exit()
             if not pm.FIX_LEARNING_RATE:
                 if step in pm.ADJUST_LR_STEPS:
                     policy_net.lr /= 2
@@ -187,3 +267,4 @@ def central_agent(net_weights_qs, net_gradients_qs, stats_qs):
                     logger.info("Learning rate is decreased to " + str(policy_net.lr) + " at step " + str(step))
             if step < pm.STEP_TRAIN_CRITIC_NET:  # set policy net lr to 0 to train critic net only
                 policy_net.lr = 0.0
+
--- parameters.py
+++ parameters.py
@@ -33,6 +33,7 @@ if TRAINING_MODE == "SL":
 MODEL_DIR = "Models/"  # checkpoint dir
 SUMMARY_DIR = "TensorBoard/"  # tensorboard logging dir , we could use
 DISP_INTERVAL = 50  # display frequency
+NUM_RECORD_AGENTS = 2  # log details of 2 agents in tensorboard and ignore others for saved space
 SKIP_FIRST_VAL = True  # if False, the central agent will test the initialized model at first before training
 SELECT_ACTION_MAX_PROB = False  # whether to select the action with the highest probability or select based on distribution, default based on distribution
 MASK_PROB = 1.  # whether to mask actions mapped None jobs, set it to be lower seems to be worse
@@ -41,6 +42,8 @@ ASSIGN_BUNDLE = True  # assign 1 ps and 1 worker for each in the beginning of ea
 # hyperparameters
 SL_LOSS_FUNCTION = "Cross_Entropy"  # "Mean_Square", "Cross_Entropy", "Absolute_Difference"
 OPTIMIZER = "Adam"  # RMSProp
+FIX_LEARNING_RATE = True  # keep constant learning rate
+ADJUST_LR_STEPS = [5000]  # halving learning rate once reaching a certain step, not functional
 if TRAINING_MODE == "SL":
     LEARNING_RATE = 0.005
 else:
@@ -50,13 +53,10 @@ MINI_BATCH_SIZE = 256 // NUM_AGENTS  # python2 could use / but py3 needs //
 EPSILON_GREEDY = False  # whether to enable epsilon greedy policy for exploration
 FIX_ENTROPY_WEIGHT = True  # if true, the entropy weight is ENTROPY_WEIGHT; else, it is calculated based on ANNEALING_TEMPERATURE and MAX_ENTROPY_WEIGHT
 
-
-
-
 RAND_RANGE = 100000
 TOT_NUM_STEPS = 1000000
 if TRAINING_MODE == "SL":
-    TOT_NUM_STEPS = 100 # 10 for debug, 100000
+    TOT_NUM_STEPS = 100  # 10 for debug, 100000
 
 VAL_ON_MASTER = True  # validation on agent uses CPU instead of GPU, and may cause use up all memory, do not know why, so far it must be set true
 NUM_UNCOMPLETED_JOB_REWARD = False  # set the reward to be the number of uncompleted jobs
@@ -68,6 +68,8 @@ VARYING_SKIP_NUM_WORKERS = True
 MIN_ACTION_PROB_FOR_SKIP = 10 ** (-20)  # 10**(-12)
 
 VARYING_PS_WORKER_RATIO = True  # explore different ratio of ps over worker
+STEP_TRAIN_CRITIC_NET = 0  # number of steps for pretraining critic network, default 0, not functional
+
 JOB_RESR_BALANCE = True
 FINE_GRAIN_JCT = True
 
@@ -103,6 +105,8 @@ if TESTBED:
     SCHED_WINDOW_SIZE = 4
 VAL_DATASET = 10  # number of traces for validation in each agent
 MAX_TS_LEN = 1000  # maximal timeslot length for one trace
+
+
 # neural network
 JOB_ORDER_SHUFFLE = False
 PS_WORKER = True  # whether consider ps and worker tasks separately or not
